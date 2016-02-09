ff5a2df2b61068a529d2732f053b1f31183eb00f
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-02-09 11:03:16 +0530

    Create chapter2-pattern.js

---
+++ chapter2-pattern.js
@@ -0,0 +1,19 @@
+function getoutput()
+{
+
+    var	a = 1;
+    var b=" ";
+    do 
+    {
+
+
+       b = b +"#";
+       console.log(b);
+       a = a + 1;
+
+     }while(a<=7);
+
+}
+
+
+getoutput();
