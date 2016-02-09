059754de08437cba14cec8b0350527cf0f3849a7
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-02-09 11:05:35 +0530

    Create chapter3-minfunction.js

---
+++ chapter3-minfunction.js
@@ -0,0 +1,21 @@
+function min (a,b)
+{
+  if (a>b)
+  {
+  	console.log(b);
+  	return b;
+  }
+  else if (a==b)
+  {
+     consloe.log(a);
+     return a;
+  }
+  else
+  {
+  	console.log (a);
+  	return a;
+  }
+
+}
+
+min (2,5);
